5b43feb83313f56c5c5ec0698adf27daca66498b
Ben Guo <jianning.guo@pactera.com>
2021-03-29 18:04:01 +0800

    Improve test.sh

--- demo-record/src/main/resources/demo/shell/test.sh
+++ demo-record/src/main/resources/demo/shell/test.sh
@@ -5,73 +5,61 @@ YELLOW="\033[1;33m"
 
 C_RESET="\033[0m"
 
-start_sh(){
-  echo -e "${GREEN}prepare merge video ana audio...${C_RESET}"
+# 显示启动 banner
+start_banner() {
+  echo -e "${YELLOW}prepare merge video ana audio...${C_RESET}"
 }
 
-start_sh
 resource_path=""
 output_path=""
-# f - file 源文件所在的目录
-# o - output 输出目录
-analysisOpt(){
-  echo "function analysisOpt parameter is:"${*}
+filename=""
+
+analysisOpt() {
   # getopts 是等于处理函数内容只是帮忙完成了函数的转换
   while getopts ":f:o:m:" OPT; do
     case $OPT in
-    f) resource_path=$OPTARG;;
-    o) output_path=$OPTARG;;
-    m) filename=$OPTARG;;
+    # f - file 源文件所在的目录
+    f) resource_path=$OPTARG ;;
+    # o - output 输出目录
+    o) output_path=$OPTARG ;;
+    # m - 生成的文件名称前缀
+    m) filename=$OPTARG ;;
     esac
   done
 }
+start_banner
 analysisOpt "$@"
 
+# 错误信息 List
 error_list=()
-if [ -d ${resource_path} ];then
-  echo "Resource Path is $resource_path.";
+# 空字符串为 0, Shell 中 0 为 true，其他值为 false
+if [[ "x${resource_path}" != "x" ]] && [ -d ${resource_path} ]; then
+  echo -e "Resource Path is ${GREEN}$resource_path${C_RESET}."
 else
-  error_list[${#error_list[*]}]="Can't have ${RED}Resource${C_RESET} file path."
+  error_list[${#error_list[*]}]="${YELLOW}Need '-f' provider ${RED}Resource${C_RESET} ${YELLOW}file path.${C_RESET}"
 fi
 
-if [ -d ${output_path} ]; then
-    echo "Output Path is $output_path。"
+if [[ "x${output_path}" != "x" ]] && [ -d ${output_path} ]; then
+  echo -e "Output Path is ${GREEN}$output_path${C_RESET}。"
 else
-  error_list[${#error_list[*]}]="${YELLOW}Can't have ${RED}Dist${C_RESET} ${YELLOW}file path.${C_RESET}"
+  error_list[${#error_list[*]}]="${YELLOW}Need '-o' provider ${RED}Dist${C_RESET} ${YELLOW}file path.${C_RESET}"
 fi
 
-error_flag=false;
-for msg in "${error_list[*]}"
-do
-  error_flag=true;
-  echo -e "\a"$msg;
+error_flag=false
+for msg in "${error_list[@]}"; do
+  error_flag=true
+  echo -e $msg
 done
-if ${erro_flag}; then
-    exit 1;
+# 不符合要求，错误推出 shell
+if [[ "$error_flag" == "true" ]]; then
+  echo -e "Error! exit!"
+  exit 1
 fi
+
+# 遍历文件
+
+
 # 指向视频文件
 video_f=""
 # 指向音频文件
 audio_f=""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
