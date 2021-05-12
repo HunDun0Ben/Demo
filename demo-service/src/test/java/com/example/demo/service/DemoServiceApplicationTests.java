1fbfea1ee50aa50ed14ac215ac739cabaab7fddb
Ben Guo <jianning.guo@pactera.com>
2021-05-12 17:53:41 +0800

    Junit 降级，统一使用 Junit4．Junit 编码。

--- demo-service/src/test/java/com/example/demo/service/DemoServiceApplicationTests.java
+++ demo-service/src/test/java/com/example/demo/service/DemoServiceApplicationTests.java
@@ -1,9 +1,11 @@
-package com.example.demoservice;
+package com.example.demo.service;
 
 import org.junit.jupiter.api.Test;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@PowerMockIgnore({"javax.*.*", "com.sun.*", "org.xml.*", "org.apache.*"})
 class DemoServiceApplicationTests {
 
 	@Test
