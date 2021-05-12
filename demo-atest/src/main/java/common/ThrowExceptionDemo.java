e0f2464570dd74f06fe32bff74f83aff420d65bd
Ben Guo <jianning.guo@pactera.com>
2021-05-12 17:06:20 +0800

    测试Demo - 函数编程 - String 相关测试 - Exception 相关测试

---
+++ demo-atest/src/main/java/common/ThrowExceptionDemo.java
@@ -0,0 +1,20 @@
+package common;
+
+public class ThrowExceptionDemo {
+
+    public static void main(String[] args){
+        try {
+            throwException();
+        } catch (Exception e) {
+            System.out.println("Function outside.");
+        }
+    }
+
+    public static void throwException(){
+        try {
+            throw new Exception("function inside.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
