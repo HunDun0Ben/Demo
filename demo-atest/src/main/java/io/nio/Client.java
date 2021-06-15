f446eb67e45b4139def0cdae92d871dfcc9a6abd
Ben Guo <jianning.guo@pactera.com>
2021-06-15 18:12:47 +0800

    add bio and nio demo

---
+++ demo-atest/src/main/java/io/nio/Client.java
@@ -0,0 +1,23 @@
+package io.nio;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class Client {
+    public static void main(String[] args){
+
+        Channel client = new NioSocketChannel();
+        // 创建默认，分配了一个 pipeline
+        ChannelPipeline pipeline = client.pipeline();
+        ChannelHandler handler = new ChannelInitializer<Channel>() {
+            @Override
+            protected void initChannel(Channel ch) throws Exception {
+                System.out.println("test channel init!");
+            }
+        };
+        client.pipeline().addFirst(handler);
+    }
+}
