b79cfcd494c119b6706464a3a33450902c106ec3
Ben Guo <jianning.guo@pactera.com>
2021-07-06 17:50:01 +0800

    design model code.

---
+++ demo-atest/src/main/java/design/model/VisitorModel.java
@@ -0,0 +1,52 @@
+package design.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class VisitorModel {
+    public static void main(String[] args){
+        Visitor v = new VisitorImpl();
+        List<Resource> list = Arrays.asList(new ResourceImpl(""));
+        for (Resource resource : list) {
+            resource.accept(v);
+        }
+    }
+}
+
+interface Visitor{
+    void visit(ResourceImpl impl);
+    // void visit(ResourceImpl2 impl);
+    // ... another type
+}
+
+class VisitorImpl implements Visitor {
+
+    @Override
+    public void visit(ResourceImpl impl) {
+        // 实现逻辑
+        System.out.println("实现逻辑");
+    }
+}
+
+abstract class Resource {
+    protected String path;
+
+    public Resource(String path) {
+        this.path = path;
+    }
+
+    abstract public void accept(Visitor visitor);
+}
+
+class ResourceImpl extends Resource {
+
+    public ResourceImpl(String path) {
+        super(path);
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+}
