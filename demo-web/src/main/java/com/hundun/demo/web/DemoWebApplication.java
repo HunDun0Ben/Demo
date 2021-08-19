bfeb99cf49ab9d23871b2c6f5f87531a84552e06
Ben Guo <jianning.guo@pactera.com>
2021-08-19 21:50:39 +0800

    再次初始化，提交

--- demo-web/src/main/java/com/hundun/demo/web/DemoWebApplication.java
+++ demo-web/src/main/java/com/hundun/demo/web/DemoWebApplication.java
@@ -1,6 +1,5 @@
 package com.hundun.demo.web;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
