f3ac435938b7cb1b52559c92f22f54d7e62329c4
Ben Guo <jianning.guo@pactera.com>
2021-10-26 17:44:38 +0800

    Spring security .

--- demo-web/src/main/java/com/hundun/demo/web/security/WebSercurityConfiguration.java
+++ demo-web/src/main/java/com/hundun/demo/web/security/WebSercurityConfiguration.java
@@ -3,36 +3,45 @@ package com.hundun.demo.web.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebSercurityConfiguration  extends WebSecurityConfigurerAdapter {
 
-    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
-    @Override
-    public AuthenticationManager authenticationManagerBean() {
-        return null;
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
+        auth.inMemoryAuthentication()
+                .withUser("user").password("user").roles("user")
+                .and().withUser("admin").password("admin").roles("admin");
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-    }
-
+    // 配置 Spring Security 的 Filter 链
     @Override
     public void configure(WebSecurity web) throws Exception {
-
+        super.configure(web);
     }
+
+    // 配置如何通过拦截器保护请求
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/user/**").hasRole("user")
+                .antMatchers("/admin/**").hasRole("admin")
+                .anyRequest().authenticated()
+                .and().formLogin().loginPage("/login").permitAll()
+                .and().csrf().disable();
+    }
+
 }
---
+++ demo-web/src/main/java/com/hundun/demo/web/security/controller/SecurityTestController.java
@@ -0,0 +1,30 @@
+package com.hundun.demo.web.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityTestController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "true";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/admin/hello")
+    public String helloAdmin() {
+        return "helloAdmin";
+    }
+
+    @GetMapping("/user/hello")
+    public String helloUser() {
+        return "helloUser";
+    }
+
+}
